623de3decde5136c480f0ceaa8581c77a8b5ae59
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-02-18 21:11:21 +0900

    add debug flag

--- lib/android_market_api/android_market.rb
+++ lib/android_market_api/android_market.rb
@@ -16,12 +16,14 @@ class AndroidMarket
   @@game_categories=Array.[]('ARCADE','BRAIN','CARDS','CASUAL','GAME_WALLPAPER','RACING','SPORTS_GAMES','GAME_WIDGETS')
   @@application_categories=Array.[]('BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','EDUCATION','ENTERTAINMENT','FINANCE','HEALTH_AND_FITNESS','LIBRARIES_AND_DEMO','LIFESTYLE','APP_WALLPAPER','MEDIA_AND_VIDEO','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRANSPORTATION','TRAVEL_AND_LOCAL','WEATHER','APP_WIDGETS')
   @@languages=Array.[]('en','pt_PT','pt_BR','es','es_419','fr','it','es')
- 
+
+  @@debug=false
+
   def AndroidMarket.get_top_selling_free_app_in_category(category,position,language='en')
     url = "https://play.google.com/store/apps/category/#{category}?start=#{position-1}&num=24&hl=#{language}"
     doc = Hpricot(open(url,'User-Agent' => 'ruby'))
     buy_div=doc.search("//div[@data-analyticsid='top-free']//div[@class='goog-inline-block carousel-cell']").first
-    puts "Getting Application package "+buy_div.attributes['data-docid']
+    puts "Getting Application package "+buy_div.attributes['data-docid'] if @@debug
     app=AndroidMarketApplication.new(buy_div.attributes['data-docid'],language)
     return app
   end
@@ -30,7 +32,7 @@ class AndroidMarket
     url = "https://play.google.com/store/apps/category/#{category}?start=#{position-1}&num=24&hl=#{language}"
     doc = Hpricot(open(url,'User-Agent' => 'ruby'))
     buy_div=doc.search("//div[@data-analyticsid='top-paid']//div[@class='goog-inline-block carousel-cell']").first
-    puts "Getting Application package "+buy_div.attributes['data-docid']
+    puts "Getting Application package "+buy_div.attributes['data-docid'] if @@debug
     app=AndroidMarketApplication.new(buy_div.attributes['data-docid'],language)
     return app
   end
@@ -40,7 +42,7 @@ class AndroidMarket
     url = "https://play.google.com/store/apps/collection/topselling_free?start=#{position-1}&num=24&hl=#{language}"
     doc = Hpricot(open(url,'User-Agent' => 'ruby'))
     buy_div=doc.search("//div[@class='num-pagination-page']//li[@class='goog-inline-block']").first
-    puts "Getting Application package "+buy_div.attributes['data-docid']
+    puts "Getting Application package "+buy_div.attributes['data-docid'] if @@debug
     app=AndroidMarketApplication.new(buy_div.attributes['data-docid'],language)
     return app
   end
@@ -49,7 +51,7 @@ class AndroidMarket
     url = "https://play.google.com/store/apps/collection/topselling_paid?start=#{position-1}&num=24&hl=#{language}"
     doc = Hpricot(open(url,'User-Agent' => 'ruby'))
     buy_div=doc.search("//div[@class='num-pagination-page']//li[@class='goog-inline-block']").first
-    puts "Getting Application package "+buy_div.attributes['data-docid']
+    puts "Getting Application package "+buy_div.attributes['data-docid'] if @@debug
     app=AndroidMarketApplication.new(buy_div.attributes['data-docid'],language)
     return app
   end
@@ -60,7 +62,7 @@ class AndroidMarket
     buy_lis=doc.search("li[@class='goog-inline-block']") 
     apps = Array.new
     buy_lis.each do |buy_li|
-      puts "Getting Application package "+buy_li.attributes['data-docid']
+      puts "Getting Application package "+buy_li.attributes['data-docid'] if @@debug
       apps << AndroidMarketApplication.new(buy_li.attributes['data-docid'],language)
     end
     return apps 
@@ -77,8 +79,10 @@ class AndroidMarket
   def AndroidMarket.get_application_categories()
     return @@application_categories
   end
-end  
 
-
-
-
+  class << self
+    def debug=(is_debug)
+      @@debug = is_debug
+    end
+  end
+end
